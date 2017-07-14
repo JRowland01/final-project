0871a6be9dae94b036d88ef34fba64f46676f5eb
JRowland01 <j_rowland01@hotmail.com>
2017-07-14 10:00:23 -0400

    updates

--- app/components/register.js
+++ app/components/register.js
@@ -28,30 +28,32 @@ class Register extends Component {
 	render() {
 		let errorMsg = <div>A registration error occured.</div>
 		return (
-			<div>
-				<h2>Register</h2>
-			{/*Register function is invoked when user clicks the submit button*/}
-				<form onSubmit={(e)=>this.register(e)}>
-					{this.state.problem ? errorMsg : null}
-					<div>
-						<label>First Name</label>
-						<input type="text" ref={(input)=> this.firstname = input}/>
-					</div>
-					<div>
-						<label>Last Name</label>
-						<input type="text" ref={(input)=> this.lastname = input}/>
-					</div>
-					<div>
-						<label>Email</label>
-						<input type="email" ref={(input)=> this.email = input}/>
-					</div>
-					<div>
-						<label>Password</label>
-						<input type="password" ref={(input)=> this.password = input}/>
-					</div>
-					<input type="submit" value="Register"/>
-				</form>
-				
+			<div className="row">
+				<div className="col-xs-4 col-xs-offset-4">
+					<h2>Register</h2>
+				{/*Register function is invoked when user clicks the submit button*/}
+					<form onSubmit={(e)=>this.register(e)}>
+						{this.state.problem ? errorMsg : null}
+						<div className="form-group pmd-textfield">
+							<label>First Name</label>
+							<input type="text" ref={(input)=> this.firstname = input} className="form-control"/>
+						</div>
+						<div className="form-group pmd-textfield">
+							<label>Last Name</label>
+							<input type="text" ref={(input)=> this.lastname = input} className="form-control"/>
+						</div>
+						<div className="form-group pmd-textfield">
+							<label>Email</label>
+							<input type="email" ref={(input)=> this.email = input} className="form-control"/>
+						</div>
+						<div className="form-group pmd-textfield">
+							<label>Password</label>
+							<input type="password" ref={(input)=> this.password = input} className="form-control"/>
+						</div>
+						<input type="submit" value="Register" className="btn btn-primary"/>
+					</form>
+					
+				</div>
 			</div>
 		);
 	}
