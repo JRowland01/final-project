bb0bed04e2ea463e2804e06355e5acfa71428e61
JRowland01 <j_rowland01@hotmail.com>
2017-09-03 16:55:25 -0400

    text and img updates

--- app/components/home.js
+++ app/components/home.js
@@ -23,19 +23,16 @@ class Home extends Component{
 			<div className="row">
 				<div className="col-md-4">
 					<h3 className="h3-heading">About Our Ministry</h3>
-					<p>Destined to Conquer provides online Bible study activities to assist women with striving for their best life through Christ. Each lesson contains interactive questions that are in-depth, introspective and intended to enrich your faith walk.</p>
-					<p>Understanding that every woman's faith walk is different, we strive to provide content ranging in a variety of subjects to impact the lives of as many women as possible.</p>
+					<p>Destined to Conquer provides online Bible study activities to assist women with striving for their best life through Christ. Each lesson contains interactive questions that are in-depth, introspective and intended to enrich your faith walk. Understanding that every woman's journey is different, we offer content ranging in a variety of subjects.</p>
 					
 				</div>
 				<div className="col-md-4">
 					<h3 className="h3-heading">Upcoming Lesson</h3>
-					<img src="/assets/images/friends-home.jpg"/>	
-					<p>The next lesson will be "The Gift of Friendship".</p>
+					<p>The next lesson will be "The Gift of Friendship". Friendship is the springboard to every other love and the foundation for every healthy relationship. It is a proven fact that healthy friendships promote longevity. Even Jesus needed friends when He walked this earth as a man. In fact, He placed great value on relationships. </p>
 				</div>
 				<div className="col-md-4">
 				    <h3 className="h3-heading">Future Site Updates</h3>
 					<p>We currently have an independent study format. In the future, we are going to have a facilitator assisting with our site, by monitoring the activities of participants and providing valuable feedback for each completed lesson. You can be assured that all of your lesson responses will still remain confidential.</p>
-					<p>Lesson forums will also be added to the site to develop more of a community setting.</p>
 				</div>
 			</div>
 		</div>
